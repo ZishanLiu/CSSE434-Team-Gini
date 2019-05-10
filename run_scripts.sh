a9d75f1282cdc51c0d200ddc4ac83f70cae45812
Chien Wang <wangc6@rose-hulman.edu>
2019-05-10 00:47:32 -0400

    Update run_scripts.sh

--- run_scripts.sh
+++ run_scripts.sh
@@ -4,37 +4,40 @@
 # $2 = hdfs output /tmp/gini/output
 # $3 = local output output
 
-rm -rf $3
-mkdir $3
+# rm -rf $3
+# mkdir $3
 hadoop fs -rm -r $2;
 hadoop fs -mkdir $2;
 
 
-# start=1980
-# num=10
-# while [ $start -le 2015 ]
-# do
-#     end=$((start + 4))
-#     echo $start 
-#     echo $end
-#     pig -x mapreduce -p start_year=${start} -p end_year=${end} -p number=${num} -p hbaseInput=$1 -p hdfsOutput=$2 scripts/highest_between.pig;
-#     hadoop fs -rm "${2}/${start}_${end}top${num}/.pig_schema"
-#     hadoop fs -getmerge "${2}/${start}_${end}top${num}" "${3}/${start}_${end}top${num}.csv"
-#     pig -x mapreduce -p start_year=${start} -p end_year=${end} -p number=${num} -p hbaseInput=$1 -p hdfsOutput=$2 scripts/lowest_between.pig;
-#     hadoop fs -rm "${2}/${start}_${end}low${num}/.pig_schema"
-#     hadoop fs -getmerge "${2}/${start}_${end}low${num}" "${3}/${start}_${end}low${num}.csv"
-#     start=$((start + 5))
-# done
+start=1980
+num=10
+while [ $start -le 2015 ]
+do
+    end=$((start + 4))
+    echo $start 
+    echo $end
+    pig -x mapreduce -p start_year=${start} -p end_year=${end} -p number=${num} -p hbaseInput=$1 -p hdfsOutput=$2 scripts/highest_between.pig;
+    hadoop fs -rm "${2}/${start}_${end}top${num}/.pig_schema"
+    hadoop fs -getmerge "${2}/${start}_${end}top${num}" "${3}/${start}_${end}top${num}.csv"
+    pig -x mapreduce -p start_year=${start} -p end_year=${end} -p number=${num} -p hbaseInput=$1 -p hdfsOutput=$2 scripts/lowest_between.pig;
+    hadoop fs -rm "${2}/${start}_${end}low${num}/.pig_schema"
+    hadoop fs -getmerge "${2}/${start}_${end}low${num}" "${3}/${start}_${end}low${num}.csv"
+    start=$((start + 5))
+done
 
 
 
-pig -x mapreduce -p hbaseInput=$1 -p hdfsOutput=$2 scripts/recent.pig
-hadoop fs -rm "${2}/recent/.pig_schema"
-hadoop fs -getmerge "${2}/recent" "${3}/recent.csv"
+# pig -x mapreduce -p hbaseInput=$1 -p hdfsOutput=$2 scripts/recent.pig
+# hadoop fs -rm "${2}/recent/.pig_schema"
+# hadoop fs -getmerge "${2}/recent" "${3}/recent.csv"
 
-pig -x mapreduce -p hbaseInput=$1 -p hdfsOutput=$2 scripts/count_records_by_year.pig
-hadoop fs -rm "${2}/records_by_year/.pig_schema" 
-hadoop fs -getmerge "${2}/records_by_year" "${3}/records_by_year.csv"
+# pig -x mapreduce -p hbaseInput=$1 -p hdfsOutput=$2 scripts/count_records_by_year.pig
+# hadoop fs -rm "${2}/records_by_year/.pig_schema" 
+# hadoop fs -getmerge "${2}/records_by_year" "${3}/records_by_year.csv"
+
+
+# below are not used for now
 
 # pig -x mapreduce -p hbaseInput=hbase://gini_index -p hdfsOutput=/tmp/gini/output scripts/count_records_by_country.pig
 # hadoop fs -rm /tmp/gini/output/records_by_country/.pig_schema
