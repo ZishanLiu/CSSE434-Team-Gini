f7d77d90c601837f5acc51596e982ca7a9289223
root <root@hadoop-m1.us-central1-f.c.aesthetic-root-235600.internal>
2019-05-14 14:31:04 +0000

    jar

---
+++ lab8/task2/task2.java
@@ -0,0 +1,263 @@
+// ORM class for table 'task2'
+// WARNING: This class is AUTO-GENERATED. Modify at your own risk.
+//
+// Debug information:
+// Generated date: Tue May 14 13:51:23 UTC 2019
+// For connector: org.apache.sqoop.manager.MySQLManager
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.lib.db.DBWritable;
+import org.apache.sqoop.lib.JdbcWritableBridge;
+import org.apache.sqoop.lib.DelimiterSet;
+import org.apache.sqoop.lib.FieldFormatter;
+import org.apache.sqoop.lib.RecordParser;
+import org.apache.sqoop.lib.BooleanParser;
+import org.apache.sqoop.lib.BlobRef;
+import org.apache.sqoop.lib.ClobRef;
+import org.apache.sqoop.lib.LargeObjectLoader;
+import org.apache.sqoop.lib.SqoopRecord;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+public class task2 extends SqoopRecord  implements DBWritable, Writable {
+  private final int PROTOCOL_VERSION = 3;
+  public int getClassFormatVersion() { return PROTOCOL_VERSION; }
+  public static interface FieldSetterCommand {    void setField(Object value);  }  protected ResultSet __cur_result_set;
+  private Map<String, FieldSetterCommand> setters = new HashMap<String, FieldSetterCommand>();
+  private void init0() {
+    setters.put("year", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        task2.this.year = (Integer)value;
+      }
+    });
+  }
+  public task2() {
+    init0();
+  }
+  private Integer year;
+  public Integer get_year() {
+    return year;
+  }
+  public void set_year(Integer year) {
+    this.year = year;
+  }
+  public task2 with_year(Integer year) {
+    this.year = year;
+    return this;
+  }
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof task2)) {
+      return false;
+    }
+    task2 that = (task2) o;
+    boolean equal = true;
+    equal = equal && (this.year == null ? that.year == null : this.year.equals(that.year));
+    return equal;
+  }
+  public boolean equals0(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof task2)) {
+      return false;
+    }
+    task2 that = (task2) o;
+    boolean equal = true;
+    equal = equal && (this.year == null ? that.year == null : this.year.equals(that.year));
+    return equal;
+  }
+  public void readFields(ResultSet __dbResults) throws SQLException {
+    this.__cur_result_set = __dbResults;
+    this.year = JdbcWritableBridge.readInteger(1, __dbResults);
+  }
+  public void readFields0(ResultSet __dbResults) throws SQLException {
+    this.year = JdbcWritableBridge.readInteger(1, __dbResults);
+  }
+  public void loadLargeObjects(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void loadLargeObjects0(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void write(PreparedStatement __dbStmt) throws SQLException {
+    write(__dbStmt, 0);
+  }
+
+  public int write(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeInteger(year, 1 + __off, 4, __dbStmt);
+    return 1;
+  }
+  public void write0(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeInteger(year, 1 + __off, 4, __dbStmt);
+  }
+  public void readFields(DataInput __dataIn) throws IOException {
+this.readFields0(__dataIn);  }
+  public void readFields0(DataInput __dataIn) throws IOException {
+    if (__dataIn.readBoolean()) { 
+        this.year = null;
+    } else {
+    this.year = Integer.valueOf(__dataIn.readInt());
+    }
+  }
+  public void write(DataOutput __dataOut) throws IOException {
+    if (null == this.year) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.year);
+    }
+  }
+  public void write0(DataOutput __dataOut) throws IOException {
+    if (null == this.year) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.year);
+    }
+  }
+  private static final DelimiterSet __outputDelimiters = new DelimiterSet((char) 44, (char) 10, (char) 0, (char) 0, false);
+  public String toString() {
+    return toString(__outputDelimiters, true);
+  }
+  public String toString(DelimiterSet delimiters) {
+    return toString(delimiters, true);
+  }
+  public String toString(boolean useRecordDelim) {
+    return toString(__outputDelimiters, useRecordDelim);
+  }
+  public String toString(DelimiterSet delimiters, boolean useRecordDelim) {
+    StringBuilder __sb = new StringBuilder();
+    char fieldDelim = delimiters.getFieldsTerminatedBy();
+    __sb.append(FieldFormatter.escapeAndEnclose(year==null?"null":"" + year, delimiters));
+    if (useRecordDelim) {
+      __sb.append(delimiters.getLinesTerminatedBy());
+    }
+    return __sb.toString();
+  }
+  public void toString0(DelimiterSet delimiters, StringBuilder __sb, char fieldDelim) {
+    __sb.append(FieldFormatter.escapeAndEnclose(year==null?"null":"" + year, delimiters));
+  }
+  private static final DelimiterSet __inputDelimiters = new DelimiterSet((char) 44, (char) 10, (char) 0, (char) 0, false);
+  private RecordParser __parser;
+  public void parse(Text __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharSequence __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(byte [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(char [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(ByteBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  private void __loadFromFields(List<String> fields) {
+    Iterator<String> __it = fields.listIterator();
+    String __cur_str = null;
+    try {
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.year = null; } else {
+      this.year = Integer.valueOf(__cur_str);
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  private void __loadFromFields0(Iterator<String> __it) {
+    String __cur_str = null;
+    try {
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.year = null; } else {
+      this.year = Integer.valueOf(__cur_str);
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  public Object clone() throws CloneNotSupportedException {
+    task2 o = (task2) super.clone();
+    return o;
+  }
+
+  public void clone0(task2 o) throws CloneNotSupportedException {
+  }
+
+  public Map<String, Object> getFieldMap() {
+    Map<String, Object> __sqoop$field_map = new HashMap<String, Object>();
+    __sqoop$field_map.put("year", this.year);
+    return __sqoop$field_map;
+  }
+
+  public void getFieldMap0(Map<String, Object> __sqoop$field_map) {
+    __sqoop$field_map.put("year", this.year);
+  }
+
+  public void setField(String __fieldName, Object __fieldVal) {
+    if (!setters.containsKey(__fieldName)) {
+      throw new RuntimeException("No such field:"+__fieldName);
+    }
+    setters.get(__fieldName).setField(__fieldVal);
+  }
+
+}
