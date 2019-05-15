3edf7d1b102865c2c019e1f2ab9bd44bf446c640
Chien Wang <wangc6@rose-hulman.edu>
2019-05-15 14:50:15 -0400

    Update add_record.sh

--- add_record.sh
+++ add_record.sh
@@ -1,8 +1,9 @@
-# $1 = filePath
+# $1 = oldfilePath
 # $2 = country name
 # $3 = country code
 # $4 = year
 # $5 = value
+# $6 = newfilePath
 # echo "{0},{1},{2},{3}" -f $2,$3,$4,$5 | add-content -path $1
-{ echo "${2},${3},${4},${5}"; cat $1; } > $1
+{ echo "${2},${3},${4},${5}"; cat $1; } > $6
 
